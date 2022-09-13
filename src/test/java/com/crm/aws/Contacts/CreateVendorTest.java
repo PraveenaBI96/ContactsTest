a52f59123718e7a4e3ec0a48609f5d775c9e5939
pande <pande@LAPTOP-7QPC96OE>
2022-09-13 16:57:18 +0530

    vendor changes done..

--- src/test/java/com/crm/aws/Contacts/CreateVendorTest.java
+++ src/test/java/com/crm/aws/Contacts/CreateVendorTest.java
@@ -5,6 +5,7 @@ public class CreateVendorTest {
 	public static void main(String[] args) {
 System.out.println("Vendor Created Successfully");
 System.out.println("vendor not created successfully");
+System.out.println("vendor died");
 	}
 
 }
