8f8b5d1ef1832505e7092c71c56e3afd7ae3c4c9
pande <pande@LAPTOP-7QPC96OE>
2022-09-13 16:08:48 +0530

    Changes done in CreateVendorTest

--- src/test/java/com/crm/aws/Contacts/CreateVendorTest.java
+++ src/test/java/com/crm/aws/Contacts/CreateVendorTest.java
@@ -4,6 +4,7 @@ public class CreateVendorTest {
 
 	public static void main(String[] args) {
 System.out.println("Vendor Created Successfully");
+System.out.println("vendor not created successfully");
 	}
 
 }
