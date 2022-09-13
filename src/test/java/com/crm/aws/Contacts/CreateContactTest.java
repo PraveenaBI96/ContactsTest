7987a1313494d85c54bee50905814083bd031eb0
pande <pande@LAPTOP-7QPC96OE>
2022-09-13 15:47:17 +0530

    CreateContact

---
+++ src/test/java/com/crm/aws/Contacts/CreateContactTest.java
@@ -0,0 +1,9 @@
+package com.crm.aws.Contacts;
+
+public class CreateContactTest {
+
+	public static void main(String[] args) {
+System.out.println("contacts created");
+	}
+
+}
---
+++ src/test/java/com/crm/aws/Contacts/CreateVendorTest.java
@@ -0,0 +1,9 @@
+package com.crm.aws.Contacts;
+
+public class CreateVendorTest {
+
+	public static void main(String[] args) {
+System.out.println("Vendor Created Successfully");
+	}
+
+}
